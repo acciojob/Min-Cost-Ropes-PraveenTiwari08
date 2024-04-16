9abb7baa94f3d8d3ae8137c67a660c42a6685c95
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-17 00:35:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-function mincost(arr) {
-    // Import priority queue library or implement one
-    const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
-
-    // Create a priority queue
-    const pq = new MinPriorityQueue();
-
-    // Insert all ropes into the priority queue
-    arr.forEach(rope => pq.enqueue(rope));
-
-    let totalCost = 0;
-
-    // Merge the ropes until there's only one left
-    while (pq.size() > 1) {
-        // Extract the two smallest ropes
-        const rope1 = pq.dequeue().element;
-        const rope2 = pq.dequeue().element;
-
-        // Calculate the cost of merging and add it to the total cost
-        const cost = rope1 + rope2;
-        totalCost += cost;
-
-        // Insert the merged rope back into the priority queue
-        pq.enqueue(cost);
-    }
-
-    // Return the total minimum cost
-    return totalCost;
+function mincost(arr)
+{ 
+//write your code here
+// return the min cost
+	 priority_queue = make_priority_queue(arr)
+    totalCost = 0
+    while size(priority_queue) > 1:
+        cost = pop(priority_queue) + pop(priority_queue)
+        totalCost += cost
+        push(priority_queue, cost)
+    return totalCost
+  
 }
 
-module.exports = mincost;
-
+module.exports=mincost;
